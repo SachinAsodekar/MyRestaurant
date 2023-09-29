431449aa4e1a36a0614644f909fe1c6002521c46
Sachin <sachinasodekar@gail.com>
2023-09-29 23:33:21 +0530

    Home Page UI MenuItemDetails Page

---
+++ src/Components/Layout/Page/MenuItems/MenuItemList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { menuItemModel } from '../../../../Interfaces';
+import { MenuItemCard } from '.';
+import { useGetMenuItemsQuery } from '../../../../Apis/menuItemApi';
+import { useDispatch } from 'react-redux';
+import { setMenuItem } from '../../../../Storage/Redux/menuItemSlice';
+
+
+function MenuItemList() {
+
+  //const[menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  const dispatch = useDispatch();
+  const {data, isLoading} = useGetMenuItemsQuery(null);
+
+  useEffect(()=>{
+    if(!isLoading){
+      dispatch(setMenuItem(data.result));
+    }
+  },[isLoading]);
+
+  if(isLoading){
+    return(
+      <div>
+        Loading
+      </div>
+    );
+  }
+
+  return (
+    <div className='container row'>
+      {data.result.length>0 && 
+        data.result.map((menuItem:menuItemModel, index:number)=>(
+          <MenuItemCard menuItem={menuItem} key={index}/>
+        ))}
+    </div>
+  )
+}
+
+export default MenuItemList
--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Footer, Header } from '../Components/Layout';
+import { Home, MenuItemDetails, NotFound } from '../Pages';
+
 
 function App() {
+
   return (
-    <div className='App'>
-      App
+    <div className='text-success'>
+      <Header />
+      <div className='pb-5'>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route 
+            path="/menuItemsDetails/:menuItemId" 
+            element={<MenuItemDetails />}
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
